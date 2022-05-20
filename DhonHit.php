f4ebdbc258f8404139afa66aa21fab26310cf003
dhonstudio <doon13@gmail.com>
2022-05-20 21:11:45 +0700

    newer DhonHit

--- DhonHit.php
+++ DhonHit.php
@@ -1,6 +1,7 @@
 <?php
 
-Class DhonHit {
+class DhonHit
+{
     public $api_url;
     public $username;
     public $password;
@@ -15,15 +16,15 @@ Class DhonHit {
     | $table is table name on hit database which you want to post every hit component (address, entity, session, source, page, and hit)
     */
     public $table;
-    
+
     public $id_address;
     public $id_entity;
     public $id_session;
     public $id_source;
 
     public function __construct()
-	{
-        $this->dhonhit =& get_instance();
+    {
+        $this->dhonhit = &get_instance();
 
         require_once 'DhonAPI.php';
         $this->dhonapi = new DhonAPI;
@@ -35,16 +36,21 @@ Class DhonHit {
 
     private function get_ip()
     {
-        $ip_address = 
-            !empty($_SERVER["HTTP_X_CLUSTER_CLIENT_IP"]) ? $_SERVER["HTTP_X_CLUSTER_CLIENT_IP"] : 
-            (!empty($_SERVER["HTTP_X_CLIENT_IP"]) ? $_SERVER["HTTP_X_CLIENT_IP"] :
-            (!empty($_SERVER["HTTP_CLIENT_IP"]) ? $_SERVER["HTTP_CLIENT_IP"] :
-            (!empty($_SERVER["HTTP_X_FORWARDED_FOR"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] :
-            (!empty($_SERVER["HTTP_X_FORWARDED"]) ? $_SERVER["HTTP_X_FORWARDED"] :
-            (!empty($_SERVER["HTTP_FORWARDED_FOR"]) ? $_SERVER["HTTP_FORWARDED_FOR"] :
-            (!empty($_SERVER["HTTP_FORWARDED"]) ? $_SERVER["HTTP_FORWARDED"] :
-            $_SERVER["REMOTE_ADDR"]
-        ))))));
+        if (ENVIRONMENT == "development") {
+            $ip_address = "127.0.0.1";
+        } else {
+            $ip_address_pre =
+                !empty($_SERVER["HTTP_X_CLUSTER_CLIENT_IP"]) ? $_SERVER["HTTP_X_CLUSTER_CLIENT_IP"] : (!empty($_SERVER["HTTP_X_CLIENT_IP"]) ? $_SERVER["HTTP_X_CLIENT_IP"] : (!empty($_SERVER["HTTP_CLIENT_IP"]) ? $_SERVER["HTTP_CLIENT_IP"] : (!empty($_SERVER["HTTP_X_FORWARDED_FOR"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] : (!empty($_SERVER["HTTP_X_FORWARDED"]) ? $_SERVER["HTTP_X_FORWARDED"] : (!empty($_SERVER["HTTP_FORWARDED_FOR"]) ? $_SERVER["HTTP_FORWARDED_FOR"] : (!empty($_SERVER["HTTP_FORWARDED"]) ? $_SERVER["HTTP_FORWARDED"] :
+                    $_SERVER["REMOTE_ADDR"]
+                ))))));
+            foreach (explode(',', $ip_address_pre) as $ip) {
+                $ip = trim($ip); // just to be safe
+
+                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false) {
+                    $ip_address = $ip;
+                }
+            }
+        }
 
         $address_av     = $this->dhonapi->get($this->db, $this->table['address'], ['ip_address' => $ip_address]);
         $this->id_address = empty($address_av) ? $this->dhonapi->post($this->db, $this->table['address'], [
@@ -67,30 +73,37 @@ Class DhonHit {
 
     private function get_session()
     {
+        $session_name = 'DShC13v';
+
         $this->load->helper('cookie');
         $this->load->helper('string');
 
+        $session_value  = random_string('alnum', 32);
         $session_cookie = array(
-            'name'   => 'DShC13v',
-            'value'  => random_string('alnum', 32),
+            'name'   => $session_name,
+            'value'  => $session_value,
             'expire' => 2 * 60 * 60,
         );
-        $session = $this->input->cookie('DShC13v') ? $this->input->cookie('DShC13v') : set_cookie($session_cookie);
+        if (!$this->input->cookie($session_name) || ($this->input->cookie($session_name) === '' || $this->input->cookie($session_name) === null)) {
+            set_cookie($session_cookie);
+        } else {
+            $session_value = $this->input->cookie($session_name);
+        }
 
-        $session_av         = $this->dhonapi->get($this->db, $this->table['session'], ['session' => $session]);
+        $session_av         = $this->dhonapi->get($this->db, $this->table['session'], ['session' => $session_value]);
         $this->id_session   = empty($session_av) ? $this->dhonapi->post($this->db, $this->table['session'], [
-            'session' => $session,
+            'session' => $session_value,
         ])['id_session'] : $session_av[0]['id_session'];
     }
 
     private function get_source()
     {
         $source = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : base_url();
-        
+
         $source_av          = $this->dhonapi->get($this->db, $this->table['source'], ['source' => $source]);
-        $this->id_source    = empty($source_av) ? (substr($source, 0, strlen(base_url())) === base_url() ? 0 : $this->dhonapi->post($this->db, $this->table['source'], [
+        $this->id_source    = empty($source_av) ? $this->dhonapi->post($this->db, $this->table['source'], [
             'source' => $source,
-        ])['id_source']) : $source_av[0]['id_source'];
+        ])['id_source'] : $source_av[0]['id_source'];
     }
 
     private function get_page()
@@ -98,9 +111,9 @@ Class DhonHit {
         if (
             $this->uri->segment(1) != 'assets'
         ) {
-            for ($i=1; $i < 100; $i++) {
+            for ($i = 1; $i < 100; $i++) {
                 if ($this->uri->segment($i)) {
-                    $page_result[] = '/'.$this->uri->segment($i);
+                    $page_result[] = '/' . $this->uri->segment($i);
                 }
             }
             $page = $this->uri->segment(1) ? implode('', $page_result) : '/home';
@@ -115,6 +128,7 @@ Class DhonHit {
     public function create_hit()
     {
         $this->dhonapi->api_url['development'] = $this->api_url['development'];
+        $this->dhonapi->api_url['testing'] = $this->api_url['testing'];
         $this->dhonapi->api_url['production'] = $this->api_url['production'];
         $this->dhonapi->username = $this->username;
         $this->dhonapi->password = $this->password;
@@ -133,7 +147,8 @@ Class DhonHit {
                 'entity'    => $this->id_entity,
                 'session'   => $this->id_session,
                 'source'    => $this->id_source,
-                'page'      => $this->id_page
+                'page'      => $this->id_page,
+                'created_at' => date('Y-m-d H:i:s', time())
             ]);
         }
     }
